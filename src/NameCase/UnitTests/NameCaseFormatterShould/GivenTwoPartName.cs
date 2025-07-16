5940adf93ed9927542f780f1a5e7ccc12d8103c4
Paul Brown <paul@paulgbrown.com>
2025-07-16 13:32:20 -0500

    Added handle for names containing dashes. This allows names like "O'Connor-LaRoche" to be formatted correctly.

---
+++ src/NameCase/UnitTests/NameCaseFormatterShould/GivenTwoPartName.cs
@@ -0,0 +1,23 @@
+﻿using PgbNameCase;
+
+namespace UnitTests.NameCaseFormatterShould;
+
+public class GivenTwoPartName
+{
+	[Theory]
+	[InlineData("James O'Connor-Barnes")]
+	[InlineData("James O'Connor-D'Amico")]
+	[InlineData("James O'Connor-LaRoche")]
+	[InlineData("James O'Connor-MacNelis")]
+	[InlineData("John Macey-MacKenzie")]
+	[InlineData("James O'Connor-MacKenzie")]
+	public void CorrectlyFormatAName(string expectedName)
+	{
+		var nameToBeFormatted = expectedName.ToLower();
+
+		var actualName = NameCaseFormatter.Format(nameToBeFormatted);
+
+		Assert.Equal(expectedName, actualName);
+	}
+
+}
